27cd2b447b3d9a052c6978d9005ad7a5a5e784e7
prathikr <prathikr@usc.edu>
2020-04-19 16:44:02 -0700

    cleaned up debugging prints

--- log_reg.cpp
+++ log_reg.cpp
@@ -54,8 +54,8 @@ void normalize_data(float** data, int n_rows, int n_features) {
 }
 
 void relabel_yvec (int* yvec, int n_rows, int modelID, int n_models) {
-    float high = 183; // 365.0 / n_models * modelID + 1;
-    float low = 0; // 365.0 / n_models * (modelID-1);
+    float high = 365.0 / n_models * modelID + 1;
+    float low = 365.0 / n_models * (modelID-1);
     for (int i = 0; i < n_rows; i++) {
         int val = yvec[i];
         if (val >= low && val < high) {
@@ -91,12 +91,6 @@ void logistic_func(float* log_func_v, float* betas, float** data, int n_rows, in
     for (int i = 0; i < n_rows; i++) {
         log_func_v[i] = 1.0 / (1.0 + exp(log_func_v[i]));
     }
-
-    cout << "logistic_func returning: ";
-    for (int i = 0; i < n_rows; i++) {
-        cout << log_func_v[i] << ' ';
-    }
-    cout << endl;
 }
 
 void log_gradient(float* gradient, float* betas, float** data, int* yvec, int n_rows, int n_features) {
@@ -107,12 +101,6 @@ void log_gradient(float* gradient, float* betas, float** data, int* yvec, int n_
         log_func_v[i] -= yvec[i];
     }
 
-    cout << "log_gradient first_calc: ";
-    for (int i = 0; i < n_rows; i++) {
-        cout << log_func_v[i] << ' ';
-    }
-    cout << endl;
-
     for (int j = 0; j < n_features; j++) { // final_calc
         float temp = 0.0;
         for (int i = 0; i < n_rows; i++) {
@@ -120,12 +108,6 @@ void log_gradient(float* gradient, float* betas, float** data, int* yvec, int n_
         }
         gradient[j] = temp;
     }
-
-    cout << "log_gradient final_calc: ";
-    for (int i = 0; i < n_features; i++) {
-        cout << gradient[i] << ' ';
-    }
-    cout << endl;
 }
 
 float cost_func(float* betas, float** data, int* yvec, int n_rows, int n_features) {
@@ -138,54 +120,50 @@ float cost_func(float* betas, float** data, int* yvec, int n_rows, int n_feature
         float step2 = (1 - yvec[i]) * log(1 - log_func_v[i]);
         total += -step1 - step2;
     }
-    cout << "cost_func returning: " << total << endl;
     return total;
 }
 
-void grad_desc(float* betas, float** data, int* yvec, float lr, int max_iters, int n_rows, int n_features) {
-    float cost = 0;
+float grad_desc(float* betas, float** data, int* yvec, float lr, int max_iters, int n_rows, int n_features) {
     int num_iter = 1;
     for (int i = 0; i < max_iters; i++) {
-        // cout << "Betas: ";
-        // for (int b = 0; b < n_features; b++) {
-        //     cout << betas[b] << ' ';
-        // }
-        // cout << endl;
-        cost = cost_func(betas, data, yvec, n_rows, n_features);
         float* gradient = new float[n_features];
         log_gradient(gradient, betas, data, yvec, n_rows, n_features);
         for (int b = 0; b < n_features; b++) { // adjust betas
             betas[b] -= lr * gradient[b];
         }
-        cout << "Betas: ";
-        for (int b = 0; b < n_features; b++) {
-            cout << betas[b] << ' ';
-        }
-        cout << endl;
-        cout << "Iteration " << num_iter << ": Cost = " << cost << endl;
         num_iter++;
     }
+    float cost = cost_func(betas, data, yvec, n_rows, n_features);
+    return cost;
 }
 
 float predict(float* betas, float** data, int* yvec, int n_rows, int n_features) {
     float* log_func_v = new float[n_rows];
     logistic_func(log_func_v, betas, data, n_rows, n_features);
-    float threshold = 0.7;
-    int correct = 0;
-    for (int i = 0; i < n_rows; i++) {
-        cout << "(" << log_func_v[i] << ", " << yvec[i] << ")";
-        if ((yvec[i] == 0 && log_func_v[i] <= threshold) || (yvec[i] == 1 && log_func_v[i] > threshold)) {
-            correct++;
+    float threshold_step = 0.05;
+
+    float optimal_percent_correct = 0;
+    for (int t = 1; t <= 20; t++) {
+        float threshold = threshold_step * t;
+        float correct = 0.0;
+        for (int i = 0; i < n_rows; i++) {
+            if ((yvec[i] == 0 && log_func_v[i] <= threshold) || (yvec[i] == 1 && log_func_v[i] > threshold)) {
+                correct++;
+            }
+        }
+        float percent_correct = correct / n_rows * 100;
+        if (percent_correct > optimal_percent_correct) {
+            optimal_percent_correct = percent_correct;
         }
     }
-    cout << endl;
-    return (float)correct / n_rows * 100;
+
+    return optimal_percent_correct;
 }
 
 int main() {
     int n_rows = 100, n_features = 26;
-    int n_models = 2, modelID = 1; // model predicting likelihood of relapse in month 4
-    int max_iters = 200;
+    int n_models = 12, modelID = 4; // model predicting likelihood of relapse in month 4
+    int max_iters = 1000000;
     float lr = 0.01;
 
     cout << "--- Loading training data...";
@@ -198,7 +176,7 @@ int main() {
     cout << " done! ---" << endl << endl;
 
     cout << "--- Loading testing data...";
-    char* testing_filename = (char*)"training_data.csv";
+    char* testing_filename = (char*)"testing_data.csv";
     float** test = new float*[n_rows]; // allocate memory for data
     for (int i = 0; i < n_rows; i++) {
         test[i] = new float[n_features];
@@ -206,34 +184,26 @@ int main() {
     loadCSV(test, testing_filename, n_rows, n_features);
     cout << " done! ---" << endl << endl;
 
-    // cout << "--- Normalizing features..." << endl;
-    // normalize_data(data, n_rows, n_features);
-    // cout << "Example row: ";
-    // for (int i = 0; i < n_features; i++) {
-    //     cout << data[0][i] << ' ';
-    // }
-    // cout << endl;
-    // cout << " done! ---" << endl << endl;
-
     cout << "--- Extracting and re-labelling predictor...";
     int* yvec = new int[n_rows];
     extract_yvec(train, yvec, n_rows);
     // relabel_yvec(yvec, n_rows, modelID, n_models);
     cout << " done! ---" << endl << endl;
 
-    cout << "--- Training Model..." << endl;
+    cout << "--- Training Model..." << flush;
     float* betas = new float[n_features];
     initialize_betas(betas, n_features);
-    grad_desc(betas, train, yvec, lr, max_iters, n_rows, n_features);
+    float optimal_cost = grad_desc(betas, train, yvec, lr, max_iters, n_rows, n_features);
     cout << " done! ---" << endl << endl;
 
+    cout << "Cost at solution: " << optimal_cost << endl;
     cout << "Learned Betas: ";
     for (int i = 0; i < n_features; i++) {
         cout << betas[i] << ' ';
     }
-    cout << endl;
+    cout << endl << endl;
 
-    cout << "--- Measuring Performance..." << endl;
+    cout << "--- Measuring Performance...";
     float percent_correct = predict(betas, test, yvec, n_rows, n_features);
     cout << endl;
     cout << " done! ---" << endl << endl;
