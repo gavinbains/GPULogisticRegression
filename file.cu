0fba420ea495cb290969211411136cfbb6aeecad
luz <luz.camacho323@gmail.com>
2020-04-20 15:37:23 -0700

    moved to function

--- file.cu
+++ file.cu
@@ -7,8 +7,6 @@
 //define N
 //define threads/block
 
-//function to be run on the device
-
 
 //FILE IO RELATED
 //max number of lines
@@ -18,20 +16,7 @@
 //max number of characters/line
 #define MAX_CHAR 300
 
-
-//on the cpu
-int main(void){
-    // array that holds all converted training data
-    float **train_data = (float **) malloc(MAX_ROWS * sizeof(float *));
-    for(int i = 0; i < MAX_ROWS; i++) {
-        train_data[i] = (float *) malloc(MAX_COLUMNS * sizeof(float));
-    }
-    for(int i = 0; i < MAX_ROWS; i++) {
-	for(int j = 0; j < MAX_COLUMNS; j++) {
-	    train_data[i][j] = 0.0f;
-	}
-    }
-    printf("done initializing. \n");
+void LoadCSV(float** training_data) {
     //assumed file is in same folder, also rename file here
     char *filename = "testing_data.csv";
     FILE *file;
@@ -40,54 +25,51 @@ int main(void){
         printf("Can't open file \n");
         return 0;
     }
-    //thinking about how we want to access and store the unparsed_data
-    //array of lines, does each line break up into array of columns?
-    //feel free to try other ways we could arrange the unparsed_data
-
-    //var to store all of the unparsed_data
+    // unparsed data straight from file
     char unparsed_data[MAX_ROWS+10][MAX_CHAR+10];
     char copying[MAX_ROWS+10];
 
-    //keep track which line we are on
+    //keep track which row we are on
     int ltracker=0;
-
     while(fgets(copying, sizeof(copying)-10, file) != 0){
         //copying the line from temp to our array
         strncpy(unparsed_data[ltracker], copying, MAX_CHAR);
         ltracker++;
     }
-
+    //closing the file
     fclose(file);
-
-    //storing the column indvidual value
     char* col_val;
-
-    //the delimiter, in this file its commas
-    const char deli[2]=",";
-
-    //data parsing complete
+    const char deli[2]=","; // delimiter
+    // parses each value in each column per row
     for (int row = 0; row < MAX_ROWS; row++){
-        //telling it to separate out a line in unparsed_data by commas
         col_val = strtok(unparsed_data[row], deli);
         for(int col  = 0; col < MAX_COLUMNS; col++) {
             col_val = strtok(NULL, deli);
             if(col_val != NULL) {
-                train_data[row][col] = atof(col_val);
+                training_data[row][col] = atof(col_val);
             }
         }
     }
+}
 
-    for(int row = 0; row < MAX_ROWS; row++) {
-        printf("Row: %f: ", row);
-        for(int col = 0; col < MAX_COLUMNS; col++) {
-            printf("%f, ", train_data[row][col]);
-        }
-        printf("\n");
-        if(row == 25) {
-            break;
-        }
+//on the cpu
+int main(void){
+    // array that holds all converted training data
+    float **train_data = (float **) malloc(MAX_ROWS * sizeof(float *));
+    for(int i = 0; i < MAX_ROWS; i++) {
+        train_data[i] = (float *) malloc(MAX_COLUMNS * sizeof(float));
     }
 
+    LoadCSV(train_data);
+    for(int i = 0; i < MAX_ROWS; i++) {
+        printf("Row %i: ", i );
+        for(int j = 0; j < MAX_COLUMNS; j++) {
+            printf("%f, ", train_data[i][j]);
+        }
+        printf("\n");
+        break;
+    }
+    //data parsing completed
 
     //TODO: store the lines in the file as arrays
     //unparsed_data[0] = [colum1, colum2, column3, .. ,columnN]
